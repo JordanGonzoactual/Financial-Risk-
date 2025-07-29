1c217dfc4e2b336ed06697fc05bcc6d8b5bc74c7
Jordan Gonzalez <jordanaigonzalez@gmail.com>
2025-07-28 20:24:49 -0400

    Improve application logging utility functions

--- frontend/utils/logging.py
+++ frontend/utils/logging.py
@@ -1,11 +1,29 @@
 import logging
+import os
 
 def get_logger(name):
     logger = logging.getLogger(name)
     if not logger.handlers:
-        logger.setLevel(logging.INFO)
-        ch = logging.StreamHandler()
+        logger.setLevel(logging.DEBUG)  # Set to DEBUG
+
+        # Create logs directory if it doesn't exist
+        log_dir = os.path.join(os.path.dirname(__file__), '..', '..', 'logs')
+        os.makedirs(log_dir, exist_ok=True)
+        log_file = os.path.join(log_dir, 'app.log')
+
+        # Formatter
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+        # Console Handler
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
         ch.setFormatter(formatter)
         logger.addHandler(ch)
+
+        # File Handler
+        fh = logging.FileHandler(log_file)
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+
     return logger
